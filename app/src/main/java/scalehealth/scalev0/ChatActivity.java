a20a421e0daa5519470afd5f1ff8f8d2e5b0c96a
Mayank Kale <kalemayank29@gmail.com>
2017-09-10 20:49:20 -0500

    nightly commit

---
+++ app/src/main/java/scalehealth/scalev0/ChatActivity.java
@@ -0,0 +1,26 @@
+package scalehealth.scalev0;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.devcenter.androiduilibrary.ChatView;
+
+//import co.intentservice.chatui.ChatView;
+//import co.intentservice.chatui.models.ChatMessage;
+
+public class ChatActivity extends AppCompatActivity {
+
+  // private ChatMessage initChat;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        //initChat = new ChatMessage("You can now start chatting with Dr. Dinesh",System.currentTimeMillis(),ChatMessage.Type.RECEIVED);
+        ChatView chatView = (ChatView) findViewById(R.id.chat_view);
+        //chatView.newMessage("You can now start chatting with Dr. Dinesh");
+        chatView.receiveMessage("You can now start chatting with Dr. Dinesh");
+
+    }
+
+}
--- app/src/main/java/scalehealth/scalev0/PatientProfileActivity.java
+++ app/src/main/java/scalehealth/scalev0/PatientProfileActivity.java
@@ -6,13 +6,26 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class PatientProfileActivity extends AppCompatActivity {
+
+    Button oncoChat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_profile);
+
+        oncoChat = (Button) findViewById(R.id.chat);
+        oncoChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent chatIntent = new Intent(PatientProfileActivity.this, ChatActivity.class);
+                startActivity(chatIntent);
+            }
+        });
     }
 
     @Override
